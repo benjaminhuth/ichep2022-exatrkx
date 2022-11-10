6f0bf30ab33731c0e0296713af2c7edb70e4c77a
Benjamin Huth <benjamin.huth@ur.de>
2022-11-10 14:25:46 +0100

    update data

--- smeared_training/make_data.sh
+++ smeared_training/make_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # One run to get all data
-../../run_datagen.sh reconstruct . 10 --digi=truth --select --overwrite_config='{ "embeddingDim": 12 }' --with_ckf --with_truthtracking
+../../run_datagen.sh reconstruct . 10 --digi=smear --select --overwrite_config='{ "embeddingDim": 12 }' --with_ckf --with_truthtracking
 mv timing.tsv timing_with_ckf_and_truthtracking.tsv
 
 # One run to record GPU memory
@@ -9,6 +9,6 @@ echo "Monitor GPU memory..."
 (nvidia-smi --query-gpu=timestamp,index,memory.total,memory.reserved,memory.free,memory.used --format=csv,nounits --loop-ms=10 --filename=gpu_memory_profile.csv) &
 NVIDIA_SMI_PID=$!
 
-../../run_datagen.sh reconstruct . 10 --digi=truth --select --overwrite_config='{ "embeddingDim": 12 }'
+../../run_datagen.sh reconstruct . 10 --digi=smear --select --overwrite_config='{ "embeddingDim": 12 }'
 
 kill -INT $NVIDIA_SMI_PID
