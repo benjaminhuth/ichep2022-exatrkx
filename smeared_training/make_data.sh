fd8d20b8cfa012725ae536d00c570cc641db57b0
Benjamin Huth <benjamin.huth@ur.de>
2022-11-11 12:21:31 +0100

    update

--- smeared_training/make_data.sh
+++ smeared_training/make_data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # One run to record GPU memory
-echo "Monitor GPU memory without selection..."
+echo "Memory & timing without selection..."
+
 (nvidia-smi --query-gpu=timestamp,index,memory.total,memory.reserved,memory.free,memory.used --format=csv,nounits --loop-ms=10 --filename=gpu_memory_profile_no_selection.csv) &
 NVIDIA_SMI_PID=$!
 
@@ -9,8 +10,22 @@ NVIDIA_SMI_PID=$!
 
 kill -INT $NVIDIA_SMI_PID
 
+mv timing.tsv timing_without_selection.tsv
 
 # One run to get all data
+echo "Memory with selection"
+
+(nvidia-smi --query-gpu=timestamp,index,memory.total,memory.reserved,memory.free,memory.used --format=csv,nounits --loop-ms=10 --filename=gpu_memory_profile_no_selection.csv) &
+NVIDIA_SMI_PID=$!
+
+../../run_datagen.sh reconstruct . 10 --digi=smear --select --overwrite_config='{ "embeddingDim": 12 }'
+
+kill -INT $NVIDIA_SMI_PID
+
+rm timing.tsv
+rm *.root
+
+# One run to get all data and timing with selection
 echo "Collect data with particle seleciton"
 ../../run_datagen.sh reconstruct . 10 --digi=smear --select --overwrite_config='{ "embeddingDim": 12 }' --with_ckf --with_truthtracking
-mv timing.tsv timing_with_ckf_and_truthtracking.tsv
+mv timing.tsv timing_with_ckf_and_truthtracking_with_selection.tsv
