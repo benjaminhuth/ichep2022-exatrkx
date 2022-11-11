54216c6c70aa733727f67f5e574a05199f369afc
Benjamin Huth <benjamin.huth@ur.de>
2022-11-11 11:47:56 +0100

    update data

--- smeared_training/make_data.sh
+++ smeared_training/make_data.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# One run to get all data
-../../run_datagen.sh reconstruct . 10 --digi=smear --select --overwrite_config='{ "embeddingDim": 12 }' --with_ckf --with_truthtracking
-mv timing.tsv timing_with_ckf_and_truthtracking.tsv
-
 # One run to record GPU memory
-echo "Monitor GPU memory..."
-(nvidia-smi --query-gpu=timestamp,index,memory.total,memory.reserved,memory.free,memory.used --format=csv,nounits --loop-ms=10 --filename=gpu_memory_profile.csv) &
+echo "Monitor GPU memory without selection..."
+(nvidia-smi --query-gpu=timestamp,index,memory.total,memory.reserved,memory.free,memory.used --format=csv,nounits --loop-ms=10 --filename=gpu_memory_profile_no_selection.csv) &
 NVIDIA_SMI_PID=$!
 
-../../run_datagen.sh reconstruct . 10 --digi=smear --select --overwrite_config='{ "embeddingDim": 12 }'
+../../run_datagen.sh reconstruct . 10 --digi=smear --overwrite_config='{ "embeddingDim": 12 }'
 
 kill -INT $NVIDIA_SMI_PID
+
+
+# One run to get all data
+echo "Collect data with particle seleciton"
+../../run_datagen.sh reconstruct . 10 --digi=smear --select --overwrite_config='{ "embeddingDim": 12 }' --with_ckf --with_truthtracking
+mv timing.tsv timing_with_ckf_and_truthtracking.tsv
--- truth_training/make_data.sh
+++ truth_training/make_data.sh
@@ -3,12 +3,3 @@
 # One run to get all data
 ../../run_datagen.sh reconstruct . 10 --digi=truth --select --overwrite_config='{ "embeddingDim": 8 }' --with_ckf --with_truthtracking
 mv timing.tsv timing_with_ckf_and_truthtracking.tsv
-
-# One run to record GPU memory
-echo "Monitor GPU memory..."
-(nvidia-smi --query-gpu=timestamp,index,memory.total,memory.reserved,memory.free,memory.used --format=csv,nounits --loop-ms=10 --filename=gpu_memory_profile.csv) &
-NVIDIA_SMI_PID=$!
-
-../../run_datagen.sh reconstruct . 10 --digi=truth --select --overwrite_config='{ "embeddingDim": 8 }'
-
-kill -INT $NVIDIA_SMI_PID
